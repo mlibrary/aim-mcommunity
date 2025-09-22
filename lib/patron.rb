6d907a69f2626177dd5098ded1d20637bb5815ed
Monique Rio <mrio@umich.edu>
2025-09-22 11:40:31 -0400

    why this order?

--- lib/patron.rb
+++ lib/patron.rb
@@ -50,10 +50,11 @@ class Patron
 
   def self.valid_for(data)
     inst_roles = inst_roles_for(data)
-    inst_roles.filter_map do |inst_role|
+    result = inst_roles.filter_map do |inst_role|
       user = for_inst_role(inst_role: inst_role, data: data)
       user if user.includable?
-    end&.first
+    end
+    result&.first
   end
 
   def self.exclude_reasons_for(data)
